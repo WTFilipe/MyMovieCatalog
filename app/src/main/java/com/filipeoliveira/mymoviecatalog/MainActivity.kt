d86f254a6b6f14592153ab0f7b15c19a44e29e97
Filipe Oliveira <filipe.oliveira@hurb.com>
2023-10-25 13:13:29 -0300

    Adicionando tema ao aplicativo

--- app/src/main/java/com/filipeoliveira/mymoviecatalog/MainActivity.kt
+++ app/src/main/java/com/filipeoliveira/mymoviecatalog/MainActivity.kt
@@ -3,14 +3,20 @@ package com.filipeoliveira.mymoviecatalog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -46,7 +52,8 @@ class MainActivity : ComponentActivity() {
                                     Text(
                                         text = stringResource(R.string.app_name),
                                     )
-                                }
+                                },
+                                colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp))
                             )
                         }
                     },
