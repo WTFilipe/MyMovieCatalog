b56c16ae975e45251e7c378e9020fe684e3de170
Filipe Oliveira <filipe.oliveira@hurb.com>
2023-11-20 13:06:41 -0300

    Adicionando Testes para o MarkMovieAsWatchedUseCaseImpl.kt

--- core/data/src/main/java/com/filipeoliveira/data/usecase/MarkMovieAsWatchedUseCaseImpl.kt
+++ core/data/src/main/java/com/filipeoliveira/data/usecase/MarkMovieAsWatchedUseCaseImpl.kt
@@ -1,6 +1,7 @@
 package com.filipeoliveira.data.usecase
 
 import com.filipeoliveira.data.MovieRepository
+import com.filipeoliveira.domain.error.InvalidMovieIdError
 import com.filipeoliveira.domain.model.Movie
 import com.filipeoliveira.domain.usecase.MarkMovieAsWatchedUseCase
 import javax.inject.Inject
@@ -8,5 +9,11 @@ import javax.inject.Inject
 class MarkMovieAsWatchedUseCaseImpl @Inject constructor(
     private val repository: MovieRepository
 ) : MarkMovieAsWatchedUseCase {
-    override suspend fun execute(movie: Movie) = repository.markMovieAsWatched(movie = movie)
+    override suspend fun execute(movie: Movie) {
+        if (movie.id >= 0) {
+            repository.markMovieAsWatched(movie = movie)
+        } else {
+            throw InvalidMovieIdError()
+        }
+    }
 }
